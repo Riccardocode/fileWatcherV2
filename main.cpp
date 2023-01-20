0ce4d1a249a2005bf33583448bdb766e25ad5de8
°Riccardo <52591461+Riccardocode@users.noreply.github.com>
2023-01-20 12:48:17 +0100

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,264 @@
+#include <iostream>    
+#include <fstream>     
+#include <sstream>
+#include <string>
+#include <filesystem>
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+#include "FileWatcher.h"
+#include "readParameter.h"
+#include "COM.h"
+
+
+
+// dichiaro funzioni prima del main
+
+bool elaboraFile(std::string filename);
+std::string matchFile(std::string filename);
+std::vector<std::string> split(std::string txt, char delimiter);
+
+//Dichiarazione variabili globali di supporto
+bool plastica = false;
+int nplastica = 0;									//Numero immagini con plastica
+int frequencetosend = 11;							//Numero di frequeza con il quale vengono inviati gli esiti della plastica. se non viene trovato un valore nel file mantiene questo valore
+int frequencesup = 0;
+bool plasticflag = false;							// vera se almeno una delle immagini in frequencetosend ha la plastica
+int nplasticafrequence = 0;
+std::fstream my_file;
+std::fstream elaboraImg;
+std::string parametri = "";
+int Counter = 0;
+std::string coordinate = "";
+int coordinateInviate = 0;
+parameters p;
+
+//Variabili per COM
+//char COM[] = "\\\\.\\COM1";
+char COM[15];
+HANDLE handleCOM;
+COMMTIMEOUTS timeout;
+int nComRate = 57600;								// Baud (Bit) rate in bits/second 
+int nComBits = 8;									// Number of bits per frame
+bool hasCOM = false;								//Usato per salvare su file in caso la COM non e' presente
+
+int main() {
+	p = caricaParametri(COM, &frequencetosend);
+	stampa(p);
+
+	//inizializza la COM, se non la trova scrive i valori su file
+	hasCOM = createCOM(&handleCOM, COM);
+	if (hasCOM) {
+		purgePort(&handleCOM);
+		SetComParms(&handleCOM, nComRate, nComBits, timeout);
+		purgePort(&handleCOM);
+
+		// Filewatch monitora la cartella di destinazione delle immagini ogni 62 millisecondi per verificare se viene depositata una nuova immagine
+		FileWatcher fw{ "c:\\camera_shots\\", std::chrono::milliseconds(62) };
+
+		fw.start([](std::string path_to_watch, FileStatus status) -> void {
+
+			if (!std::filesystem::is_regular_file(std::filesystem::path(path_to_watch)) && status != FileStatus::erased) {
+				return;
+			}
+			switch (status) {
+			case FileStatus::created:
+			{
+				std::cout << "File created: " << path_to_watch << '\n';
+				if (elaboraFile(path_to_watch)) nplasticafrequence++;
+				if (frequencesup < frequencetosend - 1) frequencesup++;
+				else {
+					my_file.open("C:\\IMUupdated\\gpsUpdate.txt", std::ios::in);
+					if (!my_file) std::cout << "No such file";
+					else {
+						getline(my_file, coordinate);
+
+						if (nplasticafrequence > 0) coordinate = "1," + coordinate + "\n";
+						else coordinate = "0," + coordinate + "\n";
+
+						std::cout << "plastica trovata: " << nplasticafrequence << std::endl;
+
+						if (sendCoordinats(&handleCOM, coordinate)) {
+							coordinateInviate++;
+							std::cout << "ok " << coordinateInviate << std::endl;
+							std::cout << coordinate << std::endl;
+						}  // invia le coordinate alla COM
+
+						//Reset delle variabili prima di iniziare un nuovo set di immagini da analizzare
+						nplasticafrequence = 0;
+						frequencesup = 0;
+						my_file.close();
+						coordinate = "";
+					}
+				}
+				break;
+			}
+			default:
+				break;
+			}
+			});
+	}
+	else {
+		std::cout << "\nGli esiti della plastica verranno salvati su file" << std::endl;
+		// Filewatch monitora la cartella di destinazione delle immagini ogni 62 millisecondi per verificare se viene depositata una nuova immagine
+		FileWatcher fw{ "c:\\camera_shots\\", std::chrono::milliseconds(62) };
+		fw.start([](std::string path_to_watch, FileStatus status) -> void {
+
+			if (!std::filesystem::is_regular_file(std::filesystem::path(path_to_watch)) && status != FileStatus::erased) {
+				return;
+			}
+			switch (status) {
+			case FileStatus::created:
+			{
+				std::cout << "File created: " << path_to_watch << '\n';
+				if (elaboraFile(path_to_watch)) nplasticafrequence++;
+				if (frequencesup < frequencetosend - 1) frequencesup++;
+				else {
+					my_file.open("C:\\IMUupdated\\gpsUpdate.txt", std::ios::in);
+					if (!my_file) std::cout << "No such file";
+					else {
+						getline(my_file, coordinate);
+
+						if (nplasticafrequence > 0) coordinate = "1," + coordinate + "\n";
+						else coordinate = "0," + coordinate + "\n";
+
+						std::cout << "plastica trovata: " << nplasticafrequence << std::endl;
+
+						elaboraImg.open("C:\\elaboraImg\\elaboraImg.txt", std::ios::app);
+						elaboraImg << coordinate;
+						std::cout << "ok coordinate salvate " << coordinate << std::endl;
+						elaboraImg.close();
+
+						//Reset delle variabili prima di iniziare un nuovo set di immagini da analizzare
+						nplasticafrequence = 0;
+						frequencesup = 0;
+						my_file.close();
+						coordinate = "";
+					}
+				}
+				break;
+			}
+			default:
+				break;
+			}
+			});
+	}
+}
+
+
+
+std::vector<std::string> split(std::string txt, char delimiter) {
+	std::istringstream ss(txt);
+	std::string token;
+	std::vector<std::string> ret;
+
+	while (std::getline(ss, token, delimiter)) {
+		ret.push_back(token);
+	}
+
+	return ret;
+}
+
+std::string matchFile(std::string filename)
+{
+	int distance = 0;
+	int cnt = 6;
+	int st1 = 2;
+	int st2 = 1;
+	int j;
+	std::string ret = "";
+	std::string path = "c:\\imudata\\";
+	auto v1 = split(filename, '-');
+	for (const auto& entry : std::filesystem::directory_iterator(path)) {
+		auto v2 = split(entry.path().string(), '-');
+		std::cout << entry.path().string();
+		j = 0;
+		while (std::stoi(v1[st1 + j]) == std::stoi(v2[st2 + j])) j++;
+
+		/*
+		i file IMU letti che sono parecchi minuti indietro rispetto all'immagine, andrebbero spostati in un altra cartella
+		In questo modo si risparmiano cicli di CPU non rileggendo sempre gli stessi file
+		*/
+		distance = abs(std::stoi(v1[8]) - std::stoi(v2[7]));
+		if (j == 6 && distance < 80) return entry.path().string();
+	}
+	return ret;
+}
+
+bool elaboraFile(std::string filename)
+{	/***************Dichiarazione variabili*****************/
+	uint16_t buffer[256][320];				//Matrice che contiene l'immagine letta in binario			
+	uint16_t cubo[256][320];				//Matrice con numero di colonne ridotto (320/uB) utilizzata 
+	//  per memorizzare le bande scalate di 256 e ridotte del bias dark
+	float classi[256][20];					//Matrice che contiene le combinazioni lineari su specifiche bande.
+	bool allarme = false;					//Allarme notifica se viene riscontrato uno dei materiali ricercati
+	int sense = 0;							//Utilizzata per confrontare ---------------- 
+	int i, j, k;								//Variabili di supporto per i cicli for
+
+	/*DEBUG: dichiarazioni dei file ofstreaming e apertura dei file per salvare le diverse matrici in file di testo
+	std::ofstream bufferFile;
+	std::ofstream cuboFile;
+	std::ofstream classiFile;
+	bufferFile.open("buffer.txt", std::ios_base::app);
+	cuboFile.open("cubo.txt", std::ios_base::app);
+	classiFile.open("classi.txt", std::ios_base::app);
+	*/
+
+	std::ifstream is(filename, std::ifstream::binary);
+	if (is) {
+		//	Leggo il file.G16L semplicemente come binario
+		//	contenente interi a 16bit per righe(256 righe da 320 colonne).
+		is.read((char*)&buffer, sizeof(buffer));
+
+		/* DEBUG:
+		Ciclo usato per creare un file di testo per la matrice Buffer
+		for (i = 0; i < 256; i++) {
+			for (j = 0; j < 320; j++) {
+				bufferFile << buffer[i][j] << " ";
+			}
+			bufferFile << "\n";
+		}
+		*/
+		for (j = 0; j <= int(320 / p.uB) - p.nBS; j++) { // ciclo sulle bande  
+
+			for (i = 0; i < 256; i++) { // ciclo sulla posizione spaziale
+				cubo[i][j] = (buffer[i][j * p.uB] / 256) - p.dark;
+				/*//cuboFile << cubo[i][j] << " "; DEBUG: usato per creare un file di testo della matrice cubo
+				//std::cout << cubo[i][j]<<"  "; DEBUG: usato per creare un file di testo della matrice cubo
+				*/
+			}
+			/*//std::cout << std::endl;			 DEBUG
+			//cuboFile << "\n";					 DEBUG
+			*/
+		}
+
+		for (i = 0; i < 256; i++) { // ciclo sulla posizione spaziale
+			for (j = 0; j < p.nC; j++) { //ciclo sui classificatori
+				classi[i][j] = p.W[j][0];
+				for (k = 0; k < p.nF; k++) { // ciclo sulle features
+					classi[i][j] = classi[i][j] + (p.W[j][k + 1] * (float)(cubo[i][p.bu[j][k]]));
+				}
+				/*
+				//classiFile << classi[i][j] << " "; DEBUG: usato per creare un file di testo della matrice classi
+				//std::cout << classi[i][j] << "  "; DEBUG: usato per stampare a video la matrice classi
+				*/
+				// all'interno dell'if se si lascia allarme = true e si commenta return true e' possibile creare tutta la matrice classi[i][j]
+				// se invece si vuole un esecuzione piu' veloce commentare allarme = true e attivare return true
+				if (classi[i][j] < 0) {
+					//OTTIMIZZAZIONE: l'utilizzo diretto del return fa risparmiare molte operazioni alla CPU 
+					//pero' la matrice classi non viene costruita.
+					// Se si vuole costruire la matrice classi, commentare return true ed attivare allarme = true
+					//allarme = true;
+					sense++;
+					if (sense >= p.tolerance)
+						return true;
+				}
+			}
+			/*
+			classiFile << "\n";					 DEBUG:usato per creare uno spazio nel file di testo di matrice classi
+			*/
+		}
+	}
+	return allarme;
+}
+
---
+++ readParameter.h
@@ -0,0 +1,213 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <cstring>
+
+
+struct parameters {
+    std::string COM_sup;
+    int tolerance;                      //Numero di pixel contenenti plastica
+    int dB;								//Numero di Righe dell'immagine - considerando che si usa una Bobcat320 il numero di righe e' 320
+    int dS;								//Numero di Colonne dell'immagine - considerando che si usa una Bobcat320 il numero di colonne e' 256
+    int uB;								//Passo di sottocampionamento delle bande
+    int dark;							//bias livello nero
+    int nF;								//Numero features usate <=40
+    int nC;								//Numero classificatori usati <=20
+    int nBS;							//Numero di Bande saltate
+    std::vector<std::vector<int>> bu;	//lista ordinata delle bande usate per ciascun classificatore: bu matrice di nC righe per nF indici, di valori compresi tra 1 e (dB div uB))
+    std::vector<std::vector<float>> W;	//matrice W dei coefficienti delle combinazioni lineari, di dimensione nC * (nF+1)
+};
+//void stampa: Stampa a video il vettore dei materiali da ricercare nell'immagine spettrale
+
+void stampa(parameters p) { // stampa tutti i parametri caricati da file
+    std::cout << "COM= " << p.COM_sup << std::endl;
+    std::cout << "dB= " << p.dB << std::endl;
+    std::cout << "dS= " << p.dS << std::endl;
+    std::cout << "uB= " << p.uB << std::endl;
+    std::cout << "dark= " << p.dark << std::endl;
+    std::cout << "nF= " << p.nF << std::endl;
+    std::cout << "nC= " << p.nC << std::endl;
+    std::cout << "nBS= " << p.nBS << std::endl;
+    for (auto iq = p.bu.begin(); iq != p.bu.end(); iq++) {
+        for (auto ic = iq->cbegin(); ic != iq->cend(); ic++) {
+            std::cout << *ic << " ";
+        }
+        std::cout << std::endl;
+
+    }
+    std::cout << std::endl;
+    for (auto iq = p.W.begin(); iq != p.W.end(); iq++) {
+        for (auto ic = iq->cbegin(); ic != iq->cend(); ic++) {
+            std::cout << *ic << " ";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+
+}
+
+
+//future implementazioni: verifica che i parametri siano idonei
+int numParam(std::string s) {
+
+    int n = 0;
+    int start = 0;
+    int stop = 0;
+    int count = 0;
+    int endpos = 0;
+    endpos = s.find(".", 0);
+    while (start != (endpos - 3)) {  // da aggiungere un controllo per l'ultima posizione
+
+        stop = s.find(",", start);
+        count = stop - start;
+        start = stop + 1;
+        n++;
+    }
+
+    return n;
+}
+
+//contamateriali non è più utilizzata
+int contamateriali(std::string s) {
+    std::fstream p;
+    std::string line;
+    int n = 0;
+    p.open(s, std::ios::in);
+    if (p.is_open()) {
+        while (getline(p, line)) {
+            if (line.substr(0, 2) != "//") n++;
+        }
+    }
+    p.close();
+    return n;
+}
+
+
+
+parameters caricaParametri(char* COMM, int* ftosend) {
+    /**************************Dichiarazione variabili**************************/
+
+    const std::string nomefile = "c:\\parametriMateriali.txt";    //nome del file da aprire. aggiungere path per mettere il file in C:?
+    std::fstream parametri;
+    std::string sup;
+    parameters p;                              //variabile di supporto per caricare i materiali dal file e poi aggiungere mat al vettore
+    int start, stop, count;
+    int endLine = 0;                              //variabili di supporto utilizzata per identificare gli elementi della stringa
+    std::vector<int> buElementi;
+    std::vector<float> wElementi;
+    int n;
+    int i = 0;                                  //variabile di supporto
+    int j = 0;                                  //variabile di supporto
+    int numParametri = 0;                       //variabile di supporto per memorizzare il numero dei parametri di una riga
+    std::string line;                           //variabile di supporto per memorizzare la riga prelevata dal file
+    std::string parametro;
+    /**************************Fine Dichiarazione variabili**************************/
+
+    /**************************Lettura parametri da file**********************************/
+    parametri.open(nomefile, std::ios::in);       //Apre il file in modalità lettura -- da aggiungere un try catch?
+    if (parametri.is_open()) {                     //esegui le istruzioni sotto se il file è stato aperto correttament
+        while (getline(parametri, line)) {         //legge una riga dal file e la memorizza nella variabile line.
+
+            if (line.substr(0, 8) == "Material") {          //se la riga inizia con il commento viene scartata
+
+                while (getline(parametri, line)) {
+                    if (line == "end") break;
+                    stop = line.find("=", 0);
+                    parametro = line.substr(0, stop);
+                    start = stop + 1;
+                    if (parametro == "COM") {
+                        stop = line.find("\n", start);
+                        p.COM_sup = "\\\\.\\" + line.substr(start, stop);
+                        for (int i = 0; i < p.COM_sup.length() + 1; i++)
+                            COMM[i] = p.COM_sup[i];
+                    }
+                    else if (parametro == "frequencetosend") {
+                        stop = line.find("\n", start);
+                        *ftosend = stoi(line.substr(start, stop));
+                    }
+                    else if (parametro == "dB") {
+                        stop = line.find("\n", start);
+                        p.dB = stoi(line.substr(start, stop));
+                    }
+                    else if (parametro == "tolerance") {
+                        stop = line.find("\n", start);
+                        p.tolerance = stoi(line.substr(start, stop));
+                    }
+                    else if (parametro == "dS") {
+                        stop = line.find("\n", start);
+                        p.dS = stoi(line.substr(start, stop));
+                    }
+                    else if (parametro == "uB") {
+                        stop = line.find("\n", start);
+                        p.uB = stoi(line.substr(start, stop));
+                    }
+                    else if (parametro == "dark") {
+                        stop = line.find("\n", start);
+                        p.dark = stoi(line.substr(start, stop));
+                    }
+                    else if (parametro == "nF") {
+                        stop = line.find("\n", start);
+                        p.nF = stoi(line.substr(start, stop));
+                    }
+                    else if (parametro == "nC") {
+                        stop = line.find("\n", start);
+                        p.nC = stoi(line.substr(start, stop));
+                    }
+                    else if (parametro == "nBS") {
+                        stop = line.find("\n", start);
+                        p.nBS = stoi(line.substr(start, stop));
+                    }
+                    else if (parametro == "bu") {
+                        endLine = line.find(".", start);
+
+                        while (stop > 0) {
+                            stop = line.find(" ", start) - 1;
+                            //if (stop < 0) break;
+
+                            buElementi.push_back(stoi(line.substr(start, stop)));
+
+                            start = stop + 2;
+
+                        }
+                        p.bu.push_back(buElementi);
+                        buElementi.clear();    // Ric:Bisogna svuotare il vettore in modo diverso?
+
+
+
+                    }
+
+                    else if (parametro == "W") {
+                        endLine = line.find(".", start);
+
+                        while (stop > 0) {
+                            stop = line.find(" ", start) - 1;
+                            //if (stop < 0) break;
+
+                            wElementi.push_back(std::stof(line.substr(start, stop)));
+
+                            start = stop + 2;
+
+                        }
+                        p.W.push_back(wElementi);
+                        wElementi.clear();    // Ric:Bisogna svuotare il vettore in modo diverso?
+                    }
+
+
+                }
+
+            }
+
+
+        }
+        parametri.close(); //close the file object.
+    }
+    else { std::cout << "file not found"; }
+    return p;
+
+    /**************************Fine Lettura parametri da file**********************************/
+
+
+
+}
